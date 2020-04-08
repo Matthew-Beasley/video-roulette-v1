dc2d4666492e8aa1b7cd150034c7f9fbfd8eb8ef
Chrys Propster <cpropster@gmail.com>
2020-04-07 18:11:44 -1000

    cors headers

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const { buildDB } = require("./dataLayer/modelsIndex");
 const { apiRouter } = require("./apiLayer/apiRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/assets", express.static(path.join(__dirname, "assets")));
 app.use("/dist", express.static(path.join(__dirname, "dist")));
 app.use("/api", apiRouter);
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 app.get("/", (req, res, next) => {
   try {
@@ -26,6 +20,7 @@ app.get("/", (req, res, next) => {
     next(error);
   }
 });
+
 //maybe rethink this error handling
 app.use((req, res, next) => {
   next({
