231f49262abb70b0266de007993231921a578a00
Micah <mhirsch144@gmail.com>
2020-04-14 17:29:40 -0700

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -2,32 +2,18 @@
 /* eslint-disable react/button-has-type */
 import React from "react";
 import { Route, Link, useHistory } from "react-router-dom";
-<<<<<<< HEAD
-import axios from "axios";
-import VideoDisplay from "./VideoDisplay";
-import Login from "./Login";
-=======
 import PairVideo from "./PairVideo";
 import GroupVideo from "./GroupVideo";
->>>>>>> dc20893f047a0dfaed42327976740112ff044de5
+import Login from "./Login";
 
 const App = () => {
   return (
     <div id="container">
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-      <Route path="/Login" render={() => <Login />} />
-=======
-      <Route path="/login" render={() => <Login />} />
->>>>>>> Stashed changes
-      <Route exact path="/" render={() => <VideoDisplay />} />
-=======
       <Link to="/pair">One on One Fun</Link>
       <Link to="/group">Join a Crowd</Link>
-      {/* <Route path="/Register" render={() => <Register />} /> */}
+      <Route path="/login" render={() => <Login />} />
       <Route path="/pair" render={() => <PairVideo />} />
       <Route path="/group" render={() => <GroupVideo />} />
->>>>>>> dc20893f047a0dfaed42327976740112ff044de5
     </div>
   );
 };
