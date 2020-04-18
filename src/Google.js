46bed8fbeae039a514e143954edfe777ee26cb63
Chrys Propster <cpropster@gmail.com>
2020-04-18 12:33:03 -1000

    login page styled

--- src/Google.js
+++ src/Google.js
@@ -1,13 +1,24 @@
 import React from "react";
 
 const Google = () => (
-  <form method="GET" action={"/api/auth"}>
-    <div className="field is-centered">
-      <p className="control has-text-centered">
-        <button type="submit">Log in with Google</button>
-      </p>
+  <div className="container">
+    <div className="row justify-content-center align-items-center">
+      <form method="GET" action="/api/auth">
+        <div className="field is-centered">
+          <p className="control has-text-centered">
+            <a className="btn btn-outline-dark" href="/api/auth" role="button">
+              <img
+                width="25px"
+                alt="Google sign-in"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
+              />{" "}
+              Login with Google
+            </a>
+          </p>
+        </div>
+      </form>
     </div>
-  </form>
+  </div>
 );
 
 export default Google;
