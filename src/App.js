fb67d553a564c49b1bb815315f2b05c3bccd7bea
Matthew Beasley <56614504+Matthew-Beasley@users.noreply.github.com>
2020-04-19 09:06:25 -0700

    Revert "Bootstrap3"

--- src/App.js
+++ src/App.js
@@ -57,12 +57,8 @@ const App = () => {
   // && history.hash === "/login"
   if (!token) {
     return (
-      <div className="container-lg h-100">
-        <div className="row h-100">
-          <div className="my-auto col-sm-12">
-            <Login />
-          </div>
-        </div>
+      <div>
+        <Login />
       </div>
     );
   } else {
