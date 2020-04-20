043b7ec3bcc308949903a77919dba50e5f66c0f9
Chrys Propster <cpropster@gmail.com>
2020-04-19 22:06:28 -1000

    changed login

--- src/Login.js
+++ src/Login.js
@@ -40,41 +40,54 @@ const Login = (handleSubmit) => {
   // };
 
   return (
-    <form
-      // onSubmit={onSubmit}
-      //substituted form action with onSubmit function
-      method="GET"
-      action={"/api/auth"}
-    >
-      <div>
-        <input
-          className="input"
-          name="email"
-          type="email"
-          value={email}
-          onChange={(ev) => setEmail(ev.target.value)}
-          placeholder="Email Address"
-        />
+    <div>
+      <div className="text-center">
+        <h1>
+          <ul className="c-rainbow">
+            <li className="c-rainbow__layer c-rainbow__layer--white">
+              PARTY WITH FRIENDS YOU'VE NEVER MET
+            </li>
+            <li className="c-rainbow__layer c-rainbow__layer--orange">
+              PARTY WITH FRIENDS YOU'VE NEVER MET
+            </li>
+            <li className="c-rainbow__layer c-rainbow__layer--red">
+              PARTY WITH FRIENDS YOU'VE NEVER MET
+            </li>
+            <li className="c-rainbow__layer c-rainbow__layer--violet">
+              PARTY WITH FRIENDS YOU'VE NEVER MET
+            </li>
+            <li className="c-rainbow__layer c-rainbow__layer--blue">
+              PARTY WITH FRIENDS YOU'VE NEVER MET
+            </li>
+            <li className="c-rainbow__layer c-rainbow__layer--green">
+              PARTY WITH FRIENDS YOU'VE NEVER MET
+            </li>
+            <li className="c-rainbow__layer c-rainbow__layer--yellow">
+              PARTY WITH FRIENDS YOU'VE NEVER MET
+            </li>
+          </ul>
+        </h1>
       </div>
-      <div>
-        <input
-          className="input"
-          name="password"
-          type="password"
-          value={password}
-          onChange={(ev) => setPassword(ev.target.value)}
-          placeholder="Password"
-        />
-      </div>
-      <div className="field is-centered">
-        <div className="has-text-centered">
-          <button type="submit">Login</button>{" "}
+      <br />
+      <form
+        className="form-inline justify-content-center"
+        method="GET"
+        action="/api/auth"
+      >
+        <div className="form-group">
+          <p>
+            <a className="btn btn-outline-dark" href="/api/auth" role="button">
+              <img
+                width="25px"
+                alt="Google sign-in"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
+              />
+              &nbsp;&nbsp;&nbsp; Login with Google
+            </a>
+          </p>
         </div>
-      </div>
-      {/* <div>
-        <Google />
-      </div> */}
-    </form>
+      </form>
+    </div>
   );
 };
 
