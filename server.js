2b39f25ab0c5d9733d656a55ea5adbb60ea00419
conbec <56614504+conbec@users.noreply.github.com>
2020-04-27 11:28:31 -0700

    refactored server.js removed auto db rebuild, added db rebuild script

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ if (!process.env.IS_PRODUCTION) {
 
 const express = require("express");
 const path = require("path");
-const cors = require("cors");
-const _ = require("lodash");
-const { buildDB } = require("./dataLayer/modelsIndex");
-const { createUser } = require("./dataLayer/modelsIndex");
+const cors = require("cors");x");
 const { apiRouter } = require("./apiLayer/apiRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -50,52 +47,5 @@ app.use((err, req, res, next) => {
     message: err.message || JSON.stringify(err),
   });
 });
-
-buildDB(); // Not crazy about blowing the db away but I guess it's best for dev
-/*const func = async () => {
-   await createUser({
-     userName: "changeMe",
-     firstName: "dave",
-     lastName: "theBrave",
-     email: "test@gmail.com",
-     password: "test",
-     googleId: "1234",
-   });
- };
- func();
-*/
-// function getLocation(position) {
-//   const latitude = position.coords.latitude;
-//   const longitude = position.coords.longitude;
-//   alert(`Latitude : ${latitude}, Longitude : ${longitude}`);
-//   function errorHandler(err) {
-//     if (err.code === 1) {
-//       alert("Error: Access is denied!");
-//     } else if (err.code === 2) {
-//       alert("Error: Position is unavailable!");
-//     }
-//   }
-
-//   function getLocation() {
-//     if (navigator.geolocation) {
-//       let options = { timeout: 60000 };
-//       navigator.geolocation.getCurrentPosition(
-//         showLocation,
-//         errorHandler,
-//         options
-//       );
-//     } else {
-//       alert("Sorry, browser does not support geolocation!");
-//     }
-//   }
-// }
-//   getLocation();
-// const geolocation = navigator.getlocation;
-// geolocation.getCurrentPosition(showLocation, errorHandler);
-// },
-//     function (error) {
-//       console.log("The locator was denied.");
-//     }
-//   );
 
 app.listen(PORT, () => console.log("Listening on PORT ", PORT));
